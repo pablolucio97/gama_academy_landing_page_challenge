0da51da050f8ba115978b585e8410f34846abbd0
pablolucio97 <pablolucio97@users.noreply.github.com>
2021-11-12 22:07:11 -0300

    Added Netlify Forms feature

--- script.js
+++ script.js
@@ -5,27 +5,40 @@ const timeMinutes = document.getElementById('minutes')
 const timeSeconds = document.getElementById('seconds')
 const subscribed = document.querySelector('.subscribed')
 
-function newSubscribe(){
-    if(input.value !== null || input.value !== ''){
-        localStorage.setItem('maratona_dev:new-subscribe', 
-        JSON.stringify(input.value))
-        if(localStorage.getItem('maratona_dev:new-subscribe')){
+function newSubscribe() {
+    if (input.value !== null || input.value !== '') {
+        localStorage.setItem('maratona_dev:new-subscribe',
+            JSON.stringify(input.value))
+        if (localStorage.getItem('maratona_dev:new-subscribe')) {
             subscribed.innerText = 'Sua inscrição foi realizada com sucesso!'
         }
     }
 }
 
-function startTime(){
+function startTime() {
     let currentSeconds = parseInt(timeSeconds.innerText)
     let currentMinutes = parseInt(timeMinutes.innerText)
+
     setInterval(() => {
-        currentSeconds --
+        currentSeconds--
         timeSeconds.innerText = currentSeconds
-        if(currentSeconds < 1){
-            currentSeconds = 59
+        console.log(currentSeconds)
+        if (currentSeconds < 1) {
+            currentSeconds = `0${currentSeconds}`
+            timeSeconds.innerText = currentSeconds
+            currentSeconds = 60
+            
             currentMinutes = currentMinutes - 1
             timeMinutes.innerText = currentMinutes
         }
+
+
+        if (currentSeconds < 10 ) {
+            currentSeconds = `0${currentSeconds}`
+            timeSeconds.innerText = currentSeconds
+        }
+
+
     }, 1000)
 }
 
